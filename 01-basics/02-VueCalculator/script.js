e5d26387dbe61e203b2a7666b97989be5ca236e9
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2023-03-25 15:22:10 +0300

    VUE-03 Calculate

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = {
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      result: 0,
+      arithmetic: 'sum',
+    };
+  },
+  methods: {
+    results() {
+      const calculate = (a, b) => {
+        if (this.arithmetic === 'sum') {
+          return this.a + this.b;
+        }
+        if (this.arithmetic === 'subtract') {
+          return this.a - this.b;
+        }
+        if (this.arithmetic === 'multiply') {
+          return this.a * this.b;
+        }
+        if (this.arithmetic === 'divide') {
+          return this.a / this.b;
+        }
+      };
+      return (this.result = calculate(this.a, this.b));
+    },
+  },
+};
+const app = createApp(App);
+app.mount('#app');
