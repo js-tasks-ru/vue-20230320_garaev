f9224e3f8ebd7bf279b2bc79a7f5cac6e0288f88
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2023-03-26 13:06:51 +0300

    VUE-04 MeetupsTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,26 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  data() {
+    return {
+      meetupId: 1,
+      meetups: '',
+    };
+  },
+  watch: {
+    meetupId: {
+      handler(id) {
+        fetchMeetupById(id).then((meetup) => {
+          this.meetups = meetup.title;
+        });
+      },
+      immediate: true,
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
+
+window.app = app;
