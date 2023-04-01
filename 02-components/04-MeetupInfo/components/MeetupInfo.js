f0cd0bab21e724187d7556773b286a01bd6b39dd
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2023-04-01 15:13:49 +0300

    VUE Components / MeetupInfo

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -1,21 +1,49 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      require: true,
+    },
+    place: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: Number,
+      require: false,
+    },
+  },
+  computed: {
+    formatDate() {
+      const date = new Date(this.date);
+      const optionFormat = { year: 'numeric', month: 'long', day: 'numeric' };
+
+      return date.toLocaleDateString(navigator.language, optionFormat);
+    },
+
+    formatISODate() {
+      const date = new Date(this.date);
+
+      return date.toISOString().substr(0, 10);
+    },
+  },
 
   template: `
     <ul class="meetup-info">
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
-      </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg"/>
+      {{ organizer }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg"/>
+      {{ place }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg"/>
+      <time :datetime="formatISODate">{{ formatDate }}</time>
+    </li>
     </ul>`,
 });
